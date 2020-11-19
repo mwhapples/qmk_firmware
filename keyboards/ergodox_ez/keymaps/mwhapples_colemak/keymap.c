fcdcfe9d19874302a06819c46b8906830e24ec6c
Michael Whapples <mwhapples@aim.com>
2020-11-19 09:44:57 +0000

    Modifiers tap in main block

--- keyboards/ergodox_ez/keymaps/mwhapples_colemak/keymap.c
+++ keyboards/ergodox_ez/keymaps/mwhapples_colemak/keymap.c
@@ -42,9 +42,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [BASE] = LAYOUT_ergodox_pretty(
   // left hand
   KC_EQL,          KC_1,        KC_2,          KC_3,    KC_4,    KC_5,    KC_LEFT,              KC_RGHT,      KC_6,    KC_7,    KC_8,    KC_9,              KC_0,           KC_MINS,
-  KC_DEL,          KC_Q,        KC_W,          KC_F,    KC_P,    KC_G,    TG(QWERTY),           TG(SYMB),     KC_J,    KC_L,    KC_U,    KC_Y,              KC_SCLN,        KC_BSLS,
-  KC_BSPC,         KC_A,        KC_R,          KC_S,    KC_T,    KC_D,                                        KC_H,    KC_N,    KC_E,    KC_I,    LT(MDIA, KC_O),           GUI_T(KC_QUOT),
-  KC_LSPO,         CTL_T(KC_Z), KC_X,          KC_C,    KC_V,    KC_B,    KC_CLCK,                  KC_INS, KC_K,    KC_M,    KC_COMM, KC_DOT,           CTL_T(KC_SLSH), KC_RSPC,
+  KC_DEL,          KC_Q,        KC_W,          KC_F,    KC_P,    KC_G,    TG(QWERTY),           TG(SYMB),     KC_J,    KC_L,    KC_U,    KC_Y,              KC_SCLN,        KC_NUBS,
+  KC_BSPC,         KC_A,        KC_R,          KC_S,    LSFT_T(KC_T),    KC_D,                                        KC_H,    RSFT_T(KC_N),    KC_E,    KC_I,    LT(MDIA, KC_O),           GUI_T(KC_QUOT),
+  KC_LSPO,         CTL_T(KC_Z), ALT_T(KC_X),          GUI_T(KC_C),    KC_V,    KC_B,    KC_CLCK,                  KC_INS, KC_K,    KC_M,    RGUI_T(KC_COMM), ALT_T(KC_DOT),           CTL_T(KC_SLSH), KC_RSPC,
   LT(SYMB,KC_GRV), KC_QUOT,     LALT(KC_LSFT), KC_LEFT, KC_RGHT,                                              KC_UP,   KC_DOWN, KC_LBRC, KC_RBRC, TT(SYMB),
                                                            ALT_T(KC_APP), KC_LGUI,                KC_LALT, CTL_T(KC_ESC),
                                                                           KC_HOME,                 KC_PGUP,
