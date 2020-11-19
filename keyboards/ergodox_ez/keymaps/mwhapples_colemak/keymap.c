e8ae3243de207790cb3ee5015f719704f376f243
Michael Whapples <mwhapples@aim.com>
2020-11-19 09:52:19 +0000

    Mod-tap for QWERTY

--- keyboards/ergodox_ez/keymaps/mwhapples_colemak/keymap.c
+++ keyboards/ergodox_ez/keymaps/mwhapples_colemak/keymap.c
@@ -75,8 +75,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   // left hand
   KC_TRNS,         KC_TRNS,     KC_TRNS,       KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,              KC_TRNS,      KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,           KC_TRNS,        KC_TRNS,
   KC_TRNS,         KC_Q,        KC_W,          KC_E,    KC_R,    KC_T,    KC_TRNS,              KC_TRNS,      KC_Y,    KC_U,    KC_I,    KC_O,              KC_P,           KC_TRNS,
-  KC_TRNS,         KC_A,        KC_S,          KC_D,    KC_F,    KC_G,                                        KC_H,    KC_J,    KC_K,    KC_L,    LT(MDIA, KC_SCLN), KC_TRNS,
-  KC_TRNS,         CTL_T(KC_Z), KC_X,          KC_C,    KC_V,    KC_B,    KC_TRNS,              KC_TRNS,      KC_N,    KC_M,    KC_COMM, KC_DOT,           CTL_T(KC_SLSH), KC_TRNS,
+  KC_TRNS,         KC_A,        KC_S,          KC_D,    LSFT_T(KC_F),    KC_G,                                        KC_H,    RSFT_T(KC_J),    KC_K,    KC_L,    LT(MDIA, KC_SCLN), KC_TRNS,
+  KC_TRNS,         CTL_T(KC_Z), ALT_T(KC_X),          GUI_T(KC_C),    KC_V,    KC_B,    KC_TRNS,              KC_TRNS,      KC_N,    KC_M,    RGUI_T(KC_COMM), ALT_T(KC_DOT),           CTL_T(KC_SLSH), KC_TRNS,
   KC_TRNS, KC_TRNS,     KC_TRNS, KC_TRNS, KC_TRNS,                                              KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
                                                            KC_TRNS, KC_TRNS,                KC_TRNS, KC_TRNS,
                                                                           KC_TRNS,                 KC_TRNS,
